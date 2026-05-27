b8f99c65650a97c3398c040f049a1aab601de24d
Yen-Chieh Huang <71396086+lashhw@users.noreply.github.com>
2026-05-27 22:39:56 +0800

    fish -> bash

---
+++ run_stedgeai.sh
@@ -0,0 +1,92 @@
+export PATH="/opt/st/stm32cubeide_2.1.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.linux64_1.0.100.202602081740/tools/bin:$PATH"
+
+STEDGEAI="/opt/ST/STEdgeAI/4.0/Utilities/linux/stedgeai"
+PROGRAMMER="/opt/st/stm32cubeide_2.1.1/plugins/com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer.linux64_2.2.400.202601091506/tools/bin/STM32_Programmer_CLI"
+
+PROJECT_DIR="$HOME/.stm32cubeaistudio/workspace/stm32f7_template/.ai/run/run-1"
+MEMPOOL_FILE="$PROJECT_DIR/.ai/mempools.json"
+BUILD_DIR="$PROJECT_DIR/STM32CubeIDE/Release"
+ELF_FILE="$BUILD_DIR/run-1.elf"
+
+MODELS_DIR="/mnt/shared/tflite"
+RESULTS_DIR="$PWD/results"
+
+TARGET="stm32f7"
+NAME="network"
+BATCH_SIZE="1"
+OPT="balanced"
+C_API="st-ai"
+SERIAL_DESC="serial:/dev/ttyACM0:115200"
+
+AFTER_FLASH_SLEEP_TIME="3s"
+
+rm -rf "$RESULTS_DIR"
+mkdir -p "$RESULTS_DIR"
+
+run_one_model() {
+    local MODEL_FILE="$1"
+    local RUN_DIR="$2"
+
+    for _ in 1; do
+        local WS_DIR="$RUN_DIR/st_ai_ws"
+        local OUT_DIR="$RUN_DIR/st_ai_output"
+
+        echo "=== GENERATE ==="
+
+        "$STEDGEAI" generate \
+            --model "$MODEL_FILE" \
+            --batch-size "$BATCH_SIZE" \
+            --mode target \
+            --optimization "$OPT" \
+            --name "$NAME" \
+            --c-api "$C_API" \
+            --target "$TARGET" \
+            --workspace "$WS_DIR" \
+            --output "$PROJECT_DIR" \
+            --memory-pool "$MEMPOOL_FILE" \
+            --quiet || break
+
+        echo "==== BUILD ===="
+
+        make -C "$BUILD_DIR" clean || break
+        make -C "$BUILD_DIR" -j8 all || break
+
+        echo "=== FLASH ==="
+
+        "$PROGRAMMER" -c port=SWD -w "$ELF_FILE" -v -rst || break
+
+        sleep "$AFTER_FLASH_SLEEP_TIME"
+
+        echo "=== VALIDATE ==="
+
+        "$STEDGEAI" validate \
+            --model "$MODEL_FILE" \
+            --batch-size "$BATCH_SIZE" \
+            --mode target \
+            --optimization "$OPT" \
+            --name "$NAME" \
+            --c-api "$C_API" \
+            --target "$TARGET" \
+            --workspace "$WS_DIR" \
+            --output "$OUT_DIR" \
+            --desc "$SERIAL_DESC" \
+            --memory-pool "$MEMPOOL_FILE" \
+            --quiet || break
+
+        echo "=== DONE: $MODEL_FILE ==="
+        return 0
+    done
+
+    echo "=== FAIL: $MODEL_FILE ==="
+    return 1
+}
+
+for MODEL_FILE in "$MODELS_DIR"/*.tflite; do
+    RUN_DIR="$RESULTS_DIR/$(basename "$MODEL_FILE" .tflite)"
+    mkdir -p "$RUN_DIR"
+    LOG_FILE="$RUN_DIR/run.log"
+    run_one_model "$MODEL_FILE" "$RUN_DIR" >> "$LOG_FILE" 2>&1
+done
+
+echo "All jobs finished."
+echo "Results are in: $RESULTS_DIR"
