d0a1798dacbbe1ca31172033d89b0ae857ff5426
Yen-Chieh Huang <71396086+lashhw@users.noreply.github.com>
2026-05-28 16:53:57 +0800

    preprocess

--- quantize_onnx.py
+++ quantize_onnx.py
@@ -1,6 +1,8 @@
 import argparse
 import numpy as np
 import onnx
+import os
+import tempfile
 
 from onnxruntime.quantization import (
     CalibrationDataReader,
@@ -8,6 +10,7 @@ from onnxruntime.quantization import (
     QuantType,
     quantize_static,
 )
+from onnxruntime.quantization.shape_inference import quant_pre_process
 
 class DummyCalibrationDataReader(CalibrationDataReader):
     def __init__(self, input_name, input_shape, num_samples):
@@ -37,25 +40,35 @@ def get_input_shape(model_input):
 
 
 def quantize_onnx_model(input_model_path, output_model_path):
-    model = onnx.load(input_model_path)
-    input_tensor = model.graph.input[0]
-    input_name = input_tensor.name
-    input_shape = get_input_shape(input_tensor)
+    with tempfile.NamedTemporaryFile(suffix=".onnx", delete=False) as f:
+        preprocessed_model_path = f.name
 
-    calib_reader = DummyCalibrationDataReader(
-        input_name=input_name,
-        input_shape=input_shape,
-        num_samples=16,
-    )
+    try:
+        quant_pre_process(input_model_path, preprocessed_model_path)
 
-    quantize_static(
-        model_input=input_model_path,
-        model_output=output_model_path,
-        calibration_data_reader=calib_reader,
-        quant_format=QuantFormat.QDQ,
-        activation_type=QuantType.QInt8,
-        weight_type=QuantType.QInt8,
-    )
+        model = onnx.load(preprocessed_model_path)
+        input_tensor = model.graph.input[0]
+        input_name = input_tensor.name
+        input_shape = get_input_shape(input_tensor)
+
+        calib_reader = DummyCalibrationDataReader(
+            input_name=input_name,
+            input_shape=input_shape,
+            num_samples=16,
+        )
+
+        quantize_static(
+            model_input=preprocessed_model_path,
+            model_output=output_model_path,
+            calibration_data_reader=calib_reader,
+            quant_format=QuantFormat.QDQ,
+            activation_type=QuantType.QInt8,
+            weight_type=QuantType.QInt8,
+            per_channel=True,
+        )
+    finally:
+        if os.path.exists(preprocessed_model_path):
+            os.remove(preprocessed_model_path)
 
 
 if __name__ == "__main__":
